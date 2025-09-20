42208909e82741d49265a52029c89d26dec9ae52
Matt Bernhard <umbernhard@gmail.com>
2025-09-20 15:26:44 -0400

    Parsing headers/body

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 
+// TODO: Optimize this
+const BUF_LEN: usize = 1024;
+
+// TODO: make this parameterized
 const PORT: &str = "8080";
 const IP: &str = "0.0.0.0";
 
+fn pretty_print_buffer(buffer: &[u8]) {
+    let s = match str::from_utf8(buffer) {
+        Ok(v) => v,
+        Err(e) => panic!("Tried to print invalid utf8 sequence {}", e),
+    };
+    println!("{}", s);
+}
+
 fn handle_client(mut stream: TcpStream) {
-    // 20-byte buffer
-    // TODO: handle more than 20 bytes at a time
     // TODO: maybe this shoud be an http server?
-    let mut buffer = [0; 20];
+    let mut buffer: [u8; BUF_LEN] = [0; BUF_LEN];
     let read_size;
 
-    // TODO: do stuff with data (echo to start?)
     match stream.read(&mut buffer) {
         Ok(size) => read_size = size,
         Err(error) => panic!("{error:?}"),
@@ -19,15 +28,31 @@ fn handle_client(mut stream: TcpStream) {
 
     println!("Received {} bytes", read_size);
 
-    if read_size > 0 {
-        // TODO use buffer
-        for i in 0..read_size {
-            println!("Data in buffer: {}", buffer[i] as char);
-        }
-        println!("");
-    } else {
-        panic!("No data!");
+    // Split into header/body
+    // This is HTTP, so a \r\n\r\n divides the header from the body
+    // Read until the first \r\n\r\n
+    let mut i = 0;
+    const WINDOW_SIZE: usize = 4;
+    let mut window: [u8; WINDOW_SIZE] = [0; WINDOW_SIZE];
+    while &window != b"\r\n\r\n" {
+        let window_idx = i + WINDOW_SIZE;
+        window = buffer[i..window_idx]
+            .try_into()
+            .expect("Received incorrect size slice");
+        i += 1;
     }
+
+    // i now points to the end of the header/start of the body
+    let header = &buffer[0..i];
+    let body = &buffer[i..];
+    println!("Found header!");
+    pretty_print_buffer(header);
+
+    println!();
+    println!("Found body!");
+    pretty_print_buffer(body);
+
+    println!("");
 }
 
 fn main() -> std::io::Result<()> {
